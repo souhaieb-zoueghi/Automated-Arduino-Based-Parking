50e7289de9bc09b73be57a88b54f3e1b00048f65
souhaieb zoueghi <sou.zoueghi@gmail.com>
2020-03-20 00:41:49 +0100

    slave commented

--- Parking_Slave/Parking_Slave.ino
+++ Parking_Slave/Parking_Slave.ino
@@ -3,14 +3,33 @@
 #include <Servo.h>
 #include <SPI.h>
 
-void turnOnRedLed();
-void turnOnGreenLed();
-void turnOffLeds();
+//MCP expander specfications
+#define MCP_GENERAL_CONF_REGISTRE 0x05
+#define MCP_WRITE_COMMAND 0x40
+#define MCP_IODIRA_REGISTRE 0x00
+#define MCP_GPIOA_REGISTRE 0x12
+
+#define CLOSE_BARRIER 90
+#define OPEN_BARRIER 0
+#define MAXIMUM_DISTANCE_TO_OPEN_BARRIER 9
+#define ARDUINO_SLAVE_ADDR 8
+
+//Get distance from the ultrasonic sensor HC-SR04
 int getDistance();
 
-//ultrasonic pins
-int pingPin = 9;
-int echoPin = 8;
+//Turn on the red led via the MCP expander
+void turnOnRedLed();
+
+//Turn on the green led via the MCP expander
+void turnOnGreenLed();
+
+//Turn off all the leds via the MCP expander
+void turnOffLeds();
+
+
+//Ultrasonic pins
+const int pingPin = 9;
+const int echoPin = 8;
 
 int numberOfCarsOut = 0;
 
@@ -21,134 +40,126 @@ ParkingStatus parkingStatus = 0;
 Servo myServo;
 
 void setup() {
-  //put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin(8);
 
-  //utilisé quand le maitre nous envoi des données (Wire.write())
+  Serial.begin(9600);
+  Wire.begin(ARDUINO_SLAVE_ADDR);
+
+  //Function called when master send data (via Wire.write())
   Wire.onReceive(receiveEvent);
 
-  //utilisé quand le maitre demande des données de l'esclave (Wire.requestfrom())
+  //Function called when master request data from slave
   Wire.onRequest(requestEvent);
 
-//Initialement la barriére est fermée
   myServo.attach(3);
-  myServo.write(90);
+  myServo.write(CLOSE_BARRIER);
 
   pinMode(SS, OUTPUT);                //configure controller's Slave Select pin to output
   digitalWrite(SS, HIGH);             //disable Slave Select
   SPI.begin();
+
   //IODIRB register is already configured to input by default
   SPI.beginTransaction(SPISettings (SPI_CLOCK_DIV8, MSBFIRST, SPI_MODE0));
 
-  //Configuration de l'expander MCP sur le Bank 0
-  digitalWrite(SS, LOW);              
-  SPI.transfer(0x40);        
-  SPI.transfer(0x05);              
-  SPI.transfer(0x00);     
+  //Configuration of MCP expander on Bank 0
+  digitalWrite(SS, LOW);
+  SPI.transfer(MCP_WRITE_COMMAND);
+  SPI.transfer(MCP_GENERAL_CONF_REGISTRE);
+  SPI.transfer(0x00);
   digitalWrite(SS, HIGH);
-                
-//Configuration du GPIOA comme sortie en ecrivant 0x00 dans le registre IODIRA (0x00)
-  digitalWrite(SS, LOW);            
-  SPI.transfer(0x40);        
-  SPI.transfer(0x00);              
-  SPI.transfer(0x00);     
-  digitalWrite(SS, HIGH);             
+
+  //Configuration of GPIOA as output(0 : output, 1 : input)
+  digitalWrite(SS, LOW);
+  SPI.transfer(MCP_WRITE_COMMAND);
+  SPI.transfer(MCP_IODIRA_REGISTRE);
+  SPI.transfer(0x00);
+  digitalWrite(SS, HIGH);
+
 }
 
 void loop() {
-  
-  // put your main code here, to run repeatedly:
 
- //il y a une voiture a la sortie du parking si la distance est <= 9
-  if(getDistance() <= 9){
-    //On léve la barriere pendant 4 secondes et on inremente le nombre des voitures sortées
+  if(getDistance() <= MAXIMUM_DISTANCE_TO_OPEN_BARRIER){
+
     numberOfCarsOut++;
-    myServo.write(0);
+    myServo.write(OPEN_BARRIER);
     delay(4000);
-    myServo.write(90);
+    myServo.write(CLOSE_BARRIER);
+
   }
-  
+
   Serial.print(" number of cars that got out : ");
-  
   Serial.println(numberOfCarsOut);
 
-  //On eteint les LEDS
   turnOffLeds();
-  
+
   delay(1000);
-  
+
 }
 
 void receiveEvent(){
 
-  //On recupere l'etat du parking du maitre 
+  //Get parking's status from the master
   parkingStatus = Wire.read();
-   
+
   if(parkingStatus == PARKING_FULL){
-    
-      turnOnRedLed();   
-      delay(2000);
-     
-  }
-  else{
-        
-      turnOnGreenLed();    
-      delay(2000);
-      
+
+      turnOnRedLed();
+      delay(1000);
+
+  }else{
+
+      turnOnGreenLed();
+      delay(1000);
+
   }
 
 }
+
 void requestEvent(){
 
-  //Si le maitre fait un requestFrom on lui envoie le nombre des voitures sortées et on le remet a 0
+  //Send the number of cars that got out to the master
   Wire.write(numberOfCarsOut);
   numberOfCarsOut = 0;
-  
+
 }
 
-//Calcul du distance
 int getDistance(){
-  
+
   int duration = 0;
   digitalWrite(pingPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(pingPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   return duration * 0.034 / 2;
- 
+
 }
 
-
 void turnOnRedLed(){
-//La LED rouge est connectée au GPIOA 7, pur l'allumer on doit ecrire 0x80 dans le registre 0x12(registre du GPIOA)
+//The red led is connected to GPIOA 7 on the MCP
       digitalWrite(SS, LOW);
-      SPI.transfer(0x40);        //read command
-      SPI.transfer(0x12);
-      SPI.transfer(0x80); 
+      SPI.transfer(MCP_WRITE_COMMAND);
+      SPI.transfer(MCP_GPIOA_REGISTRE);
+      SPI.transfer(0x80);
       digitalWrite(SS, HIGH);
-      
+
   }
 
   void turnOnGreenLed(){
-//La LED verte est connectée au GPIOA 6, pur l'allumer on doit ecrire 0x40 dans le registre 0x12(registre du GPIOA)
+//The green led is connected to GPIOA 6 on the MCP
       digitalWrite(SS, LOW);
-      SPI.transfer(0x40);        //read command
-      SPI.transfer(0x12);
-      SPI.transfer(0x40); 
+      SPI.transfer(MCP_WRITE_COMMAND);
+      SPI.transfer(MCP_GPIOA_REGISTRE);
+      SPI.transfer(0x40);
       digitalWrite(SS, HIGH);
-      
+
   }
-  
+
 void turnOffLeds(){
 //pour eteindre les leds il suffit d'ecrire 0x00 dans le registre 0x12
       digitalWrite(SS, LOW);
-      SPI.transfer(0x40);        //read command
-      SPI.transfer(0x12);
-      SPI.transfer(0x00); 
+      SPI.transfer(MCP_WRITE_COMMAND);        //read command
+      SPI.transfer(MCP_GPIOA_REGISTRE);
+      SPI.transfer(0x00);
       digitalWrite(SS, HIGH);
-      
+
   }
-
-
-    
